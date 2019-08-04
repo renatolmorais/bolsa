13043868f999916476fc5be3322d4a84fb5553a9
renatolmorais <renatolmorais@gmail.com>
2019-08-04 11:15:07 -0300

    adicionando novas configurações no config.php

--- config.php
+++ config.php
@@ -1,19 +1,48 @@
 <?php
 
 $config = array(
-"title"		=>	array(
-						"login.php"		=>	"Entrar",
-						"register.php"	=>	"Registrar",
-						"index.php"		=>	"Bolsa",
-				),
-"author"	=>	"Renato L. Morais"
+"login.php"		=>	array(
+						"title"		=>	"Entrar",
+						"banner"	=>	"Bolsa",
+						"subbanner"	=>	"Controle seus investimentos"
+					),
+"index.php"		=>	array(
+						"title"		=> "Bolsa",
+						"banner"	=> "Bolsa",
+						"subbanner"	=>	"Controle seus investimentos"
+					),
+"register.php"	=>	array(
+						"title"		=>	"Registrar",
+						"banner"	=>	"Bolsa",
+						"subbanner"	=>	"Crie um novo usuário"
+					),
+"author"		=>	"Renato L. Morais"
 );
+
+function get_page()
+{
+	return trim($_SERVER["PHP_SELF"],"\/");
+}
 
 function get_title()
 {
 	global $config;
-	$pagename = trim($_SERVER["PHP_SELF"],"\/");
-	return $config["title"][$pagename];
+	$pagename = get_page();
+	return $config[$pagename]["title"];
+}
+
+function get_banner()
+{
+	global $config;
+	$pagename = get_page();
+	return $config[$pagename]["banner"];
+}
+
+function get_subbanner()
+{
+	global $config;
+	$pagename = get_page();
+	return $config[$pagename]["subbanner"];
 }
 
 ?>
